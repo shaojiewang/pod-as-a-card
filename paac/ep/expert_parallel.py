1e090dd46e888d507688e0723e396999781953ee
shaojiewang <wsjmessi@163.com>
2024-10-30 20:28:13 +0800

    add tp and ep repo

---
+++ paac/ep/expert_parallel.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# See LICENSE for license information.
+
+import os
+import sys
+import socket
+import fcntl
+import struct
+import argparse
+import warnings
+
+import torch
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+
+from megatron.core.models.gpt.gpt_layer_specs import (
+    get_gpt_layer_local_spec,
+    get_gpt_layer_with_transformer_engine_spec,
+)
+from megatron.core.transformer.moe.moe_layer import MoELayer
+from megatron.core.transformer.moe.router import Router
+from megatron.core.transformer.transformer_config import TransformerConfig
+from megatron.training.initialize import _set_random_seed
+# from tests.unit_tests.test_utilities import Utils
+
+from .async_moe_layer import AsyncMoELayer
+
+def __train():
+    
+    pass
+
+if __name__ == "main":
+    __train()
+

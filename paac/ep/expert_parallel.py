95b83ebd8b391d6729f1e00cbed8f6a40560bdca
shaojiewang <wsjmessi@163.com>
2024-11-02 00:10:18 +0800

    run script

--- paac/ep/expert_parallel.py
+++ paac/ep/expert_parallel.py
@@ -29,9 +29,27 @@ from tests.unit_tests.test_utilities import Utils
 from moe.async_moe_layer import AsyncMoELayer
 
 def __train():
-    
+    if "OMPI_COMM_WORLD_SIZE" in os.environ:
+        # Execution with `mpirun -np N`
+        WORLD_RANK = int(os.getenv("OMPI_COMM_WORLD_RANK", "0"))
+        WORLD_SIZE = int(os.getenv("OMPI_COMM_WORLD_SIZE", "1"))
+        LOCAL_RANK = int(os.getenv("OMPI_COMM_WORLD_LOCAL_RANK", "0"))
+        LOCAL_SIZE = int(os.getenv("OMPI_COMM_WORLD_LOCAL_SIZE", "1"))
+        opts.tcp_init = True
+        opts.bind_to_device = True
+        opts.bootstrap_backend = "mpi"
+    elif "TORCHELASTIC_RUN_ID" in os.environ:
+        WORLD_RANK = int(os.getenv("RANK", "0"))
+        WORLD_SIZE = int(os.getenv("WORLD_SIZE", "1"))
+        LOCAL_RANK = int(os.getenv("LOCAL_RANK", "0"))
+        LOCAL_SIZE = int(os.getenv("LOCAL_WORLD_SIZE", "1"))
+    else:
+        raise RuntimeError(f"{__file__} must be launched with either `mpirun` or `torchrun`!")
+    NUM_NODES = WORLD_SIZE // LOCAL_SIZE
+
+    print(f"NUM_NODES={NUM_NODES}")
     pass
 
-if __name__ == "main":
+if __name__ == "__main__":
     __train()
 

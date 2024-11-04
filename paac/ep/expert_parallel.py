65272dd9bbb4e3c7b9221df05eab2a94f5fedab8
shaojiewang <wsjmessi@163.com>
2024-11-04 20:51:38 +0800

    add forward bwd code

--- paac/ep/expert_parallel.py
+++ paac/ep/expert_parallel.py
@@ -51,12 +51,13 @@ def __train():
     _set_random_seed(seed_=123, data_parallel_random_init=False)
 
     num_moe_experts = 64
+    hidden_size = 12
     moe_token_dispatcher_type = "alltoall"
     grouped_gemm = True
 
     transformer_config = TransformerConfig(
         num_layers=1,
-        hidden_size=12,
+        hidden_size=hidden_size,
         num_attention_heads=4,
         num_moe_experts=num_moe_experts,
         use_cpu_initialization=True,
@@ -65,6 +66,10 @@ def __train():
         moe_aux_loss_coeff=0.01,
         moe_grouped_gemm=grouped_gemm,
         add_bias_linear=False,
+        activation_func=torch.nn.functional.silu,
+        gated_linear_unit=True,
+        bias_activation_fusion=True,
+        moe_router_load_balancing_type="sinkhorn",
     )
     
     transformer_layer_spec = get_gpt_layer_with_transformer_engine_spec(
@@ -73,6 +78,24 @@ def __train():
     moe_layer = MoELayer(
         transformer_config, transformer_layer_spec.submodules.mlp.submodules
     )
+
+
+    moe_layer.cuda()
+    # [sequence length, batch size, hidden size]
+    seq_len = 32
+    batch_size = 2
+    hidden_states = torch.rand(
+        (seq_len, batch_size, hidden_size),
+        dtype=torch.bfloat16,
+        device="cuda",
+        requires_grad=True,
+    )
+    hidden_states.retain_grad()
+
+    output_smm, _ = moe_layer(hidden_states)
+    output_smm.mean().backward()
+
+
     Utils.destroy_model_parallel()
 
     pass
